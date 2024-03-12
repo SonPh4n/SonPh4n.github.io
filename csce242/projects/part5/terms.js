a426b08abde3f8ddbbc48dc185d7b85ffaba6416
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2024-03-12 19:07:08 -0400

    working on part5

---
+++ csce242/projects/part5/terms.js
@@ -0,0 +1,13 @@
+const getTerms = async () => {
+    const url = "https://sonph4n.github.io/csce242/projects/part5/terms.json"; // Change to the actual path of your terms.json file
+
+    try {
+        const response = await fetch(url);
+            return await response.json();
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+   
+window.onload = () => showTerms();
