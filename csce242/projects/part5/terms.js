884246db24b4e8a932b86758b18397d6583adf95
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2024-03-14 09:40:35 -0400

    assignment 13

--- csce242/projects/part5/terms.js
+++ csce242/projects/part5/terms.js
@@ -1,5 +1,5 @@
 const getTerms = async () => {
-    const url = "https://sonph4n.github.io/csce242/projects/part5/terms.json"; // Change to the actual path of your terms.json file ... file path still not working
+    const url = "https://sonph4n.github.io/csce242/projects/part5/terms.json"; 
 
     try {
         const response = await fetch(url);
