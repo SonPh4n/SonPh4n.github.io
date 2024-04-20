6b55f4199755f09519a2e50eafade03940183305
Son Phan <sonphan@Sons-MacBook-Air-2.local>
2024-04-20 12:32:35 -0400

    touch ups

--- csce242/projects/final/public/hamburger.js
+++ csce242/projects/final/public/hamburger.js
@@ -1,8 +1,23 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
+function toggleMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
   }
+}
+
+window.onload = () => {
+  const dropdown = document.getElementsByClassName("dropdown-btn");
+  for (let i = 0; i < dropdown.length; i++) {
+      dropdown[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var dropdownContent = this.nextElementSibling;
+          if (dropdownContent.style.display === "block") {
+              dropdownContent.style.display = "none";
+          } else {
+              dropdownContent.style.display = "block";
+          }
+      });
+  }
+}
